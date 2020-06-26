ef01612d86cd1de8a49dc900c014ae307ca42cb9
Bruno Poeta <brunovieirapoeta@gmail.com>
2020-06-26 00:08:21 -0300

    Adding tests to avatar component

--- projects/fabula/src/lib/components/avatar/avatar.component.spec.ts
+++ projects/fabula/src/lib/components/avatar/avatar.component.spec.ts
@@ -6,7 +6,7 @@ import { IconComponent } from '../icon/icon.component';
 describe('Avatar Component', () => {
     let component: AvatarComponent;
     let fixture: ComponentFixture<AvatarComponent>;
-  
+
     beforeEach(async(() => {
       TestBed.configureTestingModule({
         declarations: [
@@ -16,13 +16,13 @@ describe('Avatar Component', () => {
       })
         .compileComponents();
     }));
-  
+
     beforeEach(() => {
       fixture = TestBed.createComponent(AvatarComponent);
       component = fixture.componentInstance;
       fixture.detectChanges();
     });
-  
+
     it('Should create', () => {
       expect(component).toBeTruthy();
     });
@@ -30,34 +30,44 @@ describe('Avatar Component', () => {
     it('Should have an icon as an object', () => {
       component.icon = { name: 'icon' };
       fixture.detectChanges();
-  
+
       const compiled: HTMLElement = fixture.debugElement.nativeElement;
       const iconElement = compiled.querySelector('.fab-icon');
       const name = iconElement.getAttribute('data-name');
-  
+
       expect(name).toBe('icon');
     });
-  
+
     it('Should have an icon as a string', () => {
       component.icon = 'icon';
       fixture.detectChanges();
-  
+
       const compiled: HTMLElement = fixture.debugElement.nativeElement;
       const iconElement = compiled.querySelector('.fab-icon');
       const name = iconElement.getAttribute('data-name');
-  
+
       expect(name).toBe('icon');
     });
 
     it('Should show initials', () => {
       component.showInitials = 'Show Initials';
       fixture.detectChanges();
-  
+
       const compiled: HTMLElement = fixture.debugElement.nativeElement;
       const initalsElement = compiled.querySelector('.fab-avatar__initials');
       const text = initalsElement.textContent;
-  
+
       expect(text).toBe('SI');
+    });
+
+    it('Should hide icon when showing initials', () => {
+      component.showInitials = 'Show Initials';
+      fixture.detectChanges();
+
+      const compiled: HTMLElement = fixture.debugElement.nativeElement;
+      const iconElement = compiled.querySelector('.fab-icon');
+
+      expect(iconElement).toBeFalsy();
     });
 
 });
